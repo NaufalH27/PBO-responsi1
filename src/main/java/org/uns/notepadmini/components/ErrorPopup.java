12e0c948338654cf5458ad7c78f3651a83efe73d
unknown <naufalhmuzakki@gmail.com>
2024-11-05 18:46:01 +0700

    fix bug on pop ups

--- src/main/java/org/uns/notepadmini/components/ErrorPopup.java
+++ src/main/java/org/uns/notepadmini/components/ErrorPopup.java
@@ -1,5 +1,6 @@
 package org.uns.notepadmini.components;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,23 +15,27 @@ public class ErrorPopup {
 
     public ErrorPopup(Stage ownerStage, String message) {
         popupStage = new Stage();
-
+        popupStage.setTitle("Error");
         popupStage.initOwner(ownerStage);
         popupStage.initModality(Modality.APPLICATION_MODAL);
 
         Label messageLabel = new Label(message);
+        messageLabel.setWrapText(true);
+        messageLabel.setAlignment(Pos.CENTER);
+
         Button closeButton = new Button("Close");
         closeButton.setOnAction(e -> popupStage.close());
 
         VBox layout = new VBox(10);
         layout.setAlignment(Pos.CENTER);
         layout.getChildren().addAll(messageLabel, closeButton);
+        layout.setPadding(new Insets(10, 20, 10, 20));
 
         Scene scene = new Scene(layout, 300, 150);
         popupStage.setScene(scene);
     }
 
     public void show() {
-        popupStage.showAndWait(); 
+        popupStage.showAndWait();
     }
 }
--- src/main/java/org/uns/notepadmini/components/UnsavedConfirmationPopup.java
+++ src/main/java/org/uns/notepadmini/components/UnsavedConfirmationPopup.java
@@ -1,5 +1,6 @@
 package org.uns.notepadmini.components;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,17 +11,21 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class UnsavedConfirmationPopup {
-       private final Stage popupStage;
+
+    private final Stage popupStage;
     private boolean confirmed;
 
     public UnsavedConfirmationPopup(Stage ownerStage, String message) {
         popupStage = new Stage();
-        confirmed = false;  
+        popupStage.setTitle("Unsaved Warning");
+        confirmed = false;
 
         popupStage.initOwner(ownerStage);
         popupStage.initModality(Modality.APPLICATION_MODAL);
 
         Label messageLabel = new Label(message);
+        messageLabel.setWrapText(true);
+        messageLabel.setAlignment(Pos.CENTER);
 
         Button yesButton = new Button("Yes");
         yesButton.setOnAction(e -> {
@@ -36,13 +41,14 @@ public class UnsavedConfirmationPopup {
 
         VBox layout = new VBox(15, messageLabel, buttonLayout);
         layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(10, 20, 10, 20));
 
         Scene scene = new Scene(layout, 300, 150);
         popupStage.setScene(scene);
     }
 
     public boolean showAndWait() {
-        popupStage.showAndWait(); 
-        return confirmed; 
+        popupStage.showAndWait();
+        return confirmed;
     }
 }
